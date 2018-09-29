b0d8ca5ac244b5029f55f36b17d0bbeb104e663e
Arponom <31740888+Arponom@users.noreply.github.com>
2018-09-29 22:10:18 +0300

    first commit

---
+++ src/test/java/Openfile.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Openfile {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String filetoparse = "./src/test/java/fileToParse.txt";
+        openFile(filetoparse);
+    }
+//pft,fkj
+    private static void openFile(String Name) throws FileNotFoundException {
+        StringBuilder stringbuilder = new StringBuilder();
+        File file = new File(Name);
+        exists(Name);
+        String numbers;
+        List<Integer> list = new ArrayList<>();
+
+        try {
+            try (BufferedReader bufferedreader = new BufferedReader(new FileReader(file.getAbsoluteFile()))) {
+                while ((numbers = bufferedreader.readLine()) != null) {
+                    stringbuilder.append(numbers);
+                    Scanner scanner = new Scanner(numbers.replace(",", " "));
+                    while (scanner.hasNextInt()) {
+                        list.add(scanner.nextInt());
+                    }
+                    List<Integer> list1 = new ArrayList<>(list);
+                    Collections.sort(list);
+                    list1.sort(Collections.reverseOrder());
+                    System.out.println(""+list);
+                    System.out.println(""+list1);
+                }
+            }
+            }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void exists(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        if (!file.exists()){
+            throw new FileNotFoundException(file.getName());
+        }
+    }
+}
